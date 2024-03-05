7116650c899da1dd7bc9dc788062f84ad09c5d93
LX <lhxlive@qq.com>
2024-03-05 09:13:40 +0800

    处理已标记数据时有编码错误，添加跳过功能

--- mergeData.py
+++ mergeData.py
@@ -4,7 +4,9 @@ import os
 from tqdm import tqdm
 
 # 指定包含多个CSV文件的文件夹路径
-folder_path = '.\\data\\IDS2017\\MachineLearningCVE'
+folder_path = '.\\data\\IDS2017\\TrafficLabelling_'
+merged_path = 'merged_data.csv'
+
 
 # 定义处理单个CSV文件的函数
 def clean_csv(file_path):
@@ -15,7 +17,11 @@ def clean_csv(file_path):
     返回值：
     - df (DataFrame): 清洗后的DataFrame对象
     """
-    df = pd.read_csv(file_path)
+    try:
+        df = pd.read_csv(file_path)
+    except UnicodeDecodeError:
+        print(f"Skipping file due to encoding error: {file_path}")
+        return None
 
     # 去除含有NaN和Inf的行
     df = df.dropna()
@@ -23,6 +29,7 @@ def clean_csv(file_path):
     df.reset_index(drop=True, inplace=True)  # 重置索引
 
     return df
+
 
 # 定义合并多个CSV文件的函数
 def merge_csv_files(folder_path):
@@ -39,19 +46,21 @@ def merge_csv_files(folder_path):
     processed_files = tqdm(files, desc="Processing CSV files")
     for filename in processed_files:
         file_path = os.path.join(folder_path, filename)
-        # 显示当前处理的文件名及整个处理过程的进度
-        processed_files.set_postfix({"File": filename, "Progress": processed_files.n})
+        # 显示当前处理的文件名
+        processed_files.set_postfix_str(filename)
         df = clean_csv(file_path)
-        all_dfs.append(df)
+        if df is not None:
+            all_dfs.append(df)
 
     merged_df = pd.concat(all_dfs, ignore_index=True)
     return merged_df
+
 
 # 合并多个CSV文件
 merged_data = merge_csv_files(folder_path)
 
 # 将整合后的数据保存到新的CSV文件中
-merged_data.to_csv('.\\data\\IDS2017\\MachineLearningCVE\\merged_data.csv', index=False)
+merged_data.to_csv(f'{folder_path}\\{merged_path}', index=False)
 
 # 显示处理结果
 print("数据处理完成！合并后的数据已保存到 merged_data.csv 文件中。")
